4103882e180e2662e39dd66731522e5830d5e90e
Andrey Salomatin <filipovskii.off@gmail.com>
2017-12-27 21:03:25 +0100

    404 page

--- site/src/pages/404.js
+++ site/src/pages/404.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Menu from '../atoms/menu';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="page">
+    <h1 className="pageTitle">{`Code Podcast`}</h1>
+    <Menu />
+
+    <section className="pageContent">
+      <div className="postContent">
+        <h1>Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
+    </section>
   </div>
 )
 
