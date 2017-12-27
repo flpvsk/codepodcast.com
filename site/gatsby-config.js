0e8b086a8ad4ec28d814e8e631063b99afcc4843
Andrey Salomatin <filipovskii.off@gmail.com>
2017-12-27 14:46:33 +0100

    Google analytics tracking id.

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-73380046-1`
       },
     },
     {
